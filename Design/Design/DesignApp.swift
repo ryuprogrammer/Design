2b3ad6466a37536347b994c9193fb04694f2ff2b
トム・クルーズ <ryumagic@tomukuruzunoMacBook-Pro.local>
2023-05-27 22:31:01 +0900

    ファイルを追加

---
+++ Design/Design/DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignApp.swift
+//  Design
+//
+//  Created by トム・クルーズ on 2023/05/27.
+//
+
+import SwiftUI
+
+@main
+struct DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
